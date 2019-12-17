7d0a58284c40391aeaf40ca65116a4ff42021338
Josip Gracin <josip.gracin@gmail.com>
2019-12-17 09:50:37 +0100

    wip

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
             System.out.println("Hello feature1 fixes #3");
+            System.out.println("Hello feature1 fixes #3");
             SpringApplication.run(DemoApplication.class, args);
 	}
 
