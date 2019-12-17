558bfded413240acd3c5d1098fdb3a82dc0ea5c9
Josip Gracin <josip.gracin@gmail.com>
2019-12-17 15:01:40 +0100

    wip

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+            String password = "h34878";
             SpringApplication.run(DemoApplication.class, args);
 	}
 
